a9b11f7d5064ebc7a1c57b3a95137b36dae0cf73
Jared Mullins <jwmullins92@gmail.com>
2021-09-07 17:25:32 -0500

    some basic functionality and persistence with JS

--- app.js
+++ app.js
@@ -1,30 +1,186 @@
+
+// DASHBOARD - Portfolio Card element getters
+
 const totalBitcoin = document.querySelector("#total-bitcoin");
 const totalEthereum = document.querySelector("#total-ethereum");
 const totalDogecoin = document.querySelector("#total-dogecoin");
 const totalUsd = document.querySelector("#total-usd");
 const totalValue = document.querySelector("#total-value");
-const submitButton = document.querySelector("#update-portfolio");
+
+// Portfolio Card element getters
+
+// DASHBOARD - Deposit/Make Trade card element getters
+
+const depositForm = document.querySelector("#deposit-form")
 const transferFromDropdown = document.querySelector("#transfer-from");
 const transferToDropdown = document.querySelector("#transfer-to");
 const transferToAmountField = document.querySelector("#transfer-to-amount");
 const transferFromAmountField = document.querySelector("#transfer-from-amount");
 const tradeButton = document.querySelector("#trade");
-const depositForm = document.querySelector("#deposit-form")
-let isLoggedIn = false;
+
+// DASHBOARD - Deposit/Make Trade card element getters end
+
+// LOGIN - Login element getters
+
 const loginForm = document.querySelector("#login-form")
-const registrationForm = document.querySelector("#registration-form")
-const logoutButton = document.querySelector("#logout")
 const loadRegistrationButton = document.querySelector("#load-register")
+
+// LOGIN - end
+
+// REGISTRATION - Registration Element getters
+
+const registrationForm = document.querySelector("#registration-form")
+
+// REGISTRATION - end
+
+// VIEWS
 
 const dashboardView = document.querySelector("#screen-dashboard")
 const loginView = document.querySelector("#screen-login")
 const registrationView = document.querySelector("#screen-register")
 
+// VIEWS - end
+
+
+
+// NAVBARS - toggle depending on isLoggedIn boolean
+
 const loggedInNav = document.querySelector("#logged-in-nav")
 const loggedOutNav = document.querySelector("#logged-out-nav")
+let isLoggedIn = false;
+const logoutButton = document.querySelector("#logout")
+
+// NAVBARS - end
+
+// Registration Logic
+
+const loadRegistrationScreen = (e) => {
+    registrationView.classList.remove("d-none")
+    loginView.classList.add("d-none")
+    dashboardView.classList.add("d-none")
+}
+
+const registerNewUser = async (e) => {
+    e.preventDefault()
+    const firstName = document.querySelector("#firstname").value
+    const lastName = document.querySelector("#lastname").value
+    const email = document.querySelector("#email").value
+    const username = document.querySelector("#newUsername").value
+    const password = document.querySelector("#newPassword").value
+    const newUser = {
+        firstName,
+        lastName,
+        email,
+        username,
+        password
+    }
+    const user = await fetch("http://localhost:8080/api/users", {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json;charset=utf-8"
+        },
+        body: JSON.stringify(newUser)
+    })
+        .then((res) => res.json());
+    if (user) {
+        toggleLoginStatus()
+        loadPortfolioScreen(user)
+    } else {
+        console.log("Something went wrong")
+        loadLoginScreen()
+    }
+}
+
+registrationForm.addEventListener('submit', registerNewUser);
+
+// Registration End
+
+// Login Logic
+
+const loadLoginScreen = () => {
+    loginView.classList.remove("d-none")
+    dashboardView.classList.add("d-none")
+    registrationView.classList.add("d-none")
+}
+
+const verifyUser = async (e) => {
+    e.preventDefault()
+    const users = await fetch("http://localhost:8080/api/users").then((res) => res.json());
+    console.log(users)
+    const username = document.querySelector("#username");
+    const password = document.querySelector("#password");
+    for (user of users) {
+        if (user.username === username.value) {
+            if (user.password === password.value) {
+                username.value = "";
+                password.value = "";
+                toggleLoginStatus()
+                loadPortfolioScreen(user)
+                return;
+            }
+        }
+    }
+    console.log("Wrong username or password")
+}
+
+const logout = (e) => {
+    e.preventDefault;
+    isLoggedIn = false;
+    toggleLoginStatus()
+    loadLoginScreen();
+}
+
+const toggleLoginStatus = () => {
+    isLoggedIn = !isLoggedIn
+    loggedInNav.classList.toggle("d-none")
+    loggedOutNav.classList.toggle("d-none")
+}
+
+loginForm.addEventListener('submit', verifyUser);
+logoutButton.addEventListener('click', logout)
+loadRegistrationButton.addEventListener('click', loadRegistrationScreen)
+
+// Login end
 
 
-console.log(registrationForm)
+// Dashboard Start
+
+const loadPortfolioScreen = async (user) => {
+    if (isLoggedIn) {
+        dashboardView.classList.remove("d-none");
+        loginView.classList.add("d-none");
+        registrationView.classList.add("d-none");
+        await getPortfolio(2)
+    } else {
+        loadLoginScreen()
+    }
+}
+
+const getPortfolio = async (id) => {
+    const portfolio = await fetch(`http://localhost:8080/api/portfolios/${id}`).then((res) => res.json());
+    const { bitcoin, ethereum, dogecoin, usd, portfolioName } = portfolio;
+    totalBitcoin.innerHTML = parseFloat(bitcoin);
+    totalEthereum.innerHTML = parseFloat(ethereum);
+    totalDogecoin.innerHTML = parseFloat(dogecoin);
+    totalUsd.innerHTML = parseFloat(usd);
+    const name = document.querySelector("#portfolio-name");
+    name.innerText = portfolioName;
+    addTransferFromOptions(bitcoin, ethereum, dogecoin, usd)
+    calculateValue()
+}
+
+const calculateValue = async (e) => {
+    const url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,dogecoin&vs_currencies=usd";
+    const coinData = await fetch(url).then(response => response.json());
+    let bitcoin = parseFloat(totalBitcoin.innerHTML) * coinData.bitcoin.usd;
+    let ethereum = parseFloat(totalEthereum.innerHTML) * coinData.ethereum.usd;
+    let dogecoin = parseFloat(totalDogecoin.innerHTML) * coinData.dogecoin.usd;
+    let usd = parseFloat(totalUsd.innerHTML);
+    const value = (bitcoin + ethereum + dogecoin + usd).toFixed(2);
+    console.log(value);
+    totalValue.innerHTML = `${value}`;
+    // setTimeout(calculateValue, 10000)
+}
 
 const addTransferFromOptions = (bitcoin, ethereum, dogecoin, usd) => {
     const portfolioTotals = {
@@ -47,81 +203,6 @@ const addTransferFromOptions = (bitcoin, ethereum, dogecoin, usd) => {
         }
     }
 };
-
-const calculateValue = async (e) => {
-    const url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,dogecoin&vs_currencies=usd";
-    const coinData = await fetch(url).then(response => response.json());
-    let bitcoin = parseFloat(totalBitcoin.innerHTML) * coinData.bitcoin.usd;
-    let ethereum = parseFloat(totalEthereum.innerHTML) * coinData.ethereum.usd;
-    let dogecoin = parseFloat(totalDogecoin.innerHTML) * coinData.dogecoin.usd;
-    let usd = parseFloat(totalUsd.innerHTML);
-    const value = (bitcoin + ethereum + dogecoin + usd).toFixed(2);
-    console.log(value);
-    totalValue.innerHTML = `${value}`;
-    // setTimeout(calculateValue, 10000)
-}
-
-
-const updatePortfolio = async (portfolioId) => {
-    const portfolio = {
-        portfolioId,
-        bitcoin: parseFloat(totalBitcoin.innerHTML),
-        dogecoin: parseFloat(totalDogecoin.innerHTML),
-        ethereum: parseFloat(totalEthereum.innerHTML),
-        usd: parseFloat(totalUsd.innerHTML),
-        portfolioName: "Test portfolio",
-        users: [
-            {
-                userId: 1,
-                username: "Douglas",
-                password: "adfasfsa",
-                firstName: "asdfsafa",
-                lastName: "Ramirez",
-                email: "aioiiops",
-            }
-        ]
-    };
-    console.log(JSON.stringify(portfolio));
-    const response = await fetch("http://localhost:8080/api/portfolios", {
-        method: "PUT",
-        headers: {
-            'Content-Type': "application/json;charset=utf-8"
-        },
-        body: JSON.stringify(portfolio)
-    })
-        .then((res) => res.json());
-    return response;
-}
-
-const savePortfolio = async () => {
-    const portfolio = {
-        bitcoin: parseFloat(totalBitcoin.innerHTML),
-        dogecoin: parseFloat(totalDogecoin.innerHTML),
-        ethereum: parseFloat(totalEthereum.innerHTML),
-        usd: parseFloat(totalUsd.innerHTML),
-        portfolioName: "Test portfolio",
-        users: [
-            {
-                userId: 1,
-                username: "Douglas",
-                password: "adfasfsa",
-                firstName: "asdfsafa",
-                lastName: "Ramirez",
-                email: "aioiiops",
-            }
-        ]
-    };
-    const response = await fetch("http://localhost:8080/api/portfolios", {
-        method: "POST",
-        headers: {
-            'Content-Type': "application/json;charset=utf-8"
-        },
-        body: JSON.stringify(portfolio)
-    })
-        .then((res) => res.json());
-    return response;
-}
-
 
 const convertCrypto = async (e) => {
     const { value: outCurrency } = transferFromDropdown;
@@ -186,8 +267,8 @@ const makeTrade = async (e) => {
         default:
             console.log("Can't do that")
     }
-    const portfolio = await updatePortfolio(4)
-    loadPortfolio(4)
+    const portfolio = await updatePortfolio(2)
+    getPortfolio(2)
 }
 
 const makeDeposit = async (e) => {
@@ -195,118 +276,98 @@ const makeDeposit = async (e) => {
     const field = document.querySelector("#deposit-amount");
     totalUsd.innerHTML = parseFloat(totalUsd.innerHTML) + parseFloat(field.value)
     field.value = ""
-    const portfolio = await updatePortfolio(4);
-    loadPortfolio(4)
-}
-
-
-const loadPortfolio = async (id) => {
-    const portfolio = await fetch(`http://localhost:8080/api/portfolios/${id}`).then((res) => res.json());
-    const { bitcoin, ethereum, dogecoin, usd, portfolioName } = portfolio;
-    totalBitcoin.innerHTML = parseFloat(bitcoin);
-    totalEthereum.innerHTML = parseFloat(ethereum);
-    totalDogecoin.innerHTML = parseFloat(dogecoin);
-    totalUsd.innerHTML = parseFloat(usd);
-    const name = document.querySelector("#portfolio-name");
-    name.innerText = portfolioName;
-    addTransferFromOptions(bitcoin, ethereum, dogecoin, usd)
-    calculateValue()
-}
-
-const loadLoginScreen = () => {
-    loginView.classList.remove("d-none")
-    dashboardView.classList.add("d-none")
-    registrationView.classList.add("d-none")
-}
-
-const loadPortfolioScreen = (user) => {
-    if (isLoggedIn) {
-        dashboardView.classList.remove("d-none");
-        loginView.classList.add("d-none");
-        registrationView.classList.add("d-none");
-        loadPortfolio(4)
-    } else {
-        loadLoginScreen()
-    }
-}
-
-const verifyUser = async (e) => {
-    e.preventDefault()
-    const users = await fetch("http://localhost:8080/api/users").then((res) => res.json());
-    console.log(users)
-    const username = document.querySelector("#username");
-    const password = document.querySelector("#password");
-    for (user of users) {
-        if (user.username === username.value) {
-            if (user.password === password.value) {
-                username.value = "";
-                password.value = "";
-                toggleLoginStatus()
-                loadPortfolioScreen(user)
-                return;
-            }
-        }
-    }
-    console.log("Wrong username or password")
-}
-
-const logout = (e) => {
-    e.preventDefault;
-    isLoggedIn = false;
-    toggleLoginStatus()
-    loadLoginScreen();
-}
-
-const toggleLoginStatus = () => {
-    isLoggedIn = !isLoggedIn
-    loggedInNav.classList.toggle("d-none")
-    loggedOutNav.classList.toggle("d-none")
-}
-
-const loadRegistrationScreen = (e) => {
-    registrationView.classList.remove("d-none")
-    loginView.classList.add("d-none")
-    dashboardView.classList.add("d-none")
-}
-
-const registerNewUser = async (e) => {
-    e.preventDefault()
-    const firstName = document.querySelector("#firstname").value
-    const lastName = document.querySelector("#lastname").value
-    const email = document.querySelector("#email").value
-    const username = document.querySelector("#newUsername").value
-    const password = document.querySelector("#newPassword").value
-    const newUser = {
-        firstName,
-        lastName,
-        email,
-        username,
-        password
-    }
-
-    const user = await fetch("http://localhost:8080/api/users", {
-        method: "POST",
-        headers: {
-            'Content-Type': "application/json;charset=utf-8"
-        },
-        body: JSON.stringify(newUser)
-    })
-        .then((res) => res.json());
-    if (user) {
-        toggleLoginStatus()
-        loadPortfolioScreen(user)
-    } else {
-        console.log("Something went wrong")
-        loadLoginScreen()
-    }
+    const portfolio = await updatePortfolio(2);
+    getPortfolio(2)
 }
 
 transferFromAmountField.addEventListener("keyup", convertCrypto);
 tradeButton.addEventListener('click', makeTrade);
 depositForm.addEventListener('submit', makeDeposit);
-loginForm.addEventListener('submit', verifyUser);
-registrationForm.addEventListener('submit', registerNewUser);
-logoutButton.addEventListener('click', logout)
-loadRegistrationButton.addEventListener('click', loadRegistrationScreen)
 
-// calculator.addEventListener("click", calculateValue)
+// Dashboard End
+
+// Persistence Functions
+
+const updatePortfolio = async (portfolioId) => {
+    const portfolio = {
+        portfolioId,
+        bitcoin: parseFloat(totalBitcoin.innerHTML),
+        dogecoin: parseFloat(totalDogecoin.innerHTML),
+        ethereum: parseFloat(totalEthereum.innerHTML),
+        usd: parseFloat(totalUsd.innerHTML),
+        portfolioName: "Test portfolio",
+        users: [
+            {
+                userId: 1,
+                username: "Douglas",
+                password: "adfasfsa",
+                firstName: "asdfsafa",
+                lastName: "Ramirez",
+                email: "aioiiops",
+            }
+        ]
+    };
+    console.log(JSON.stringify(portfolio));
+    const response = await fetch("http://localhost:8080/api/portfolios", {
+        method: "PUT",
+        headers: {
+            'Content-Type': "application/json;charset=utf-8"
+        },
+        body: JSON.stringify(portfolio)
+    })
+        .then((res) => res.json());
+    return response;
+}
+
+const savePortfolio = async () => {
+    const portfolio = {
+        bitcoin: parseFloat(totalBitcoin.innerHTML),
+        dogecoin: parseFloat(totalDogecoin.innerHTML),
+        ethereum: parseFloat(totalEthereum.innerHTML),
+        usd: parseFloat(totalUsd.innerHTML),
+        portfolioName: "Test portfolio",
+        users: [
+            {
+                userId: 1,
+                username: "Douglas",
+                password: "adfasfsa",
+                firstName: "asdfsafa",
+                lastName: "Ramirez",
+                email: "aioiiops",
+            }
+        ]
+    };
+    const response = await fetch("http://localhost:8080/api/portfolios", {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json;charset=utf-8"
+        },
+        body: JSON.stringify(portfolio)
+    })
+        .then((res) => res.json());
+    return response;
+}
+
+// Persistence End
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
