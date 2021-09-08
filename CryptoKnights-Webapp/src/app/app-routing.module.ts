4be098d478ce38176962ed4ff1ff1bce59d6d478
cptvictor <victorsyanwenliu19@gmail.com>
2021-09-08 15:41:57 -0400

    added semicolons for formatting

--- CryptoKnights-Webapp/src/app/app-routing.module.ts
+++ CryptoKnights-Webapp/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component'
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { TestComponent } from './test/test.component';
-import { TestHomeComponent } from './test-home/test-home.component'
+import { TestHomeComponent } from './test-home/test-home.component';
 
 const routes: Routes = [
   { path: '', component: TestHomeComponent },
