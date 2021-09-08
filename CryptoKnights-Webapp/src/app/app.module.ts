cbece62ae3d75e6eac5a91f95bb7fe65eb6d4d96
cptvictor <victorsyanwenliu19@gmail.com>
2021-09-08 01:09:14 -0400

    npm stuff?

--- CryptoKnights-Webapp/src/app/app.module.ts
+++ CryptoKnights-Webapp/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewPortfolioComponent } from './new-portfolio/new-portfolio.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewPortfolioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
